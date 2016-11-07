156e18c1c2110479763513e6488485e49f92ed86
TUM-E21-ThinFilms <alexander.book@frm2.tum.de>
2016-11-07 11:36:47 +0100

    adding missing import

--- tpg26x/driver.py
+++ tpg26x/driver.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from protocol import PfeifferTPG26xProtocol
+from protocol import PfeifferTPG26xProtocol, CommunicationError
 from slave.driver import Driver, Command, _load, _typelist
 from slave.types import Mapping, Stream, Float, String, Integer
 
